289fdea57b5d8b0a470a2fddc91a90dfdf600c5e
wellington <wellingtonslpbi@hotmail.com>
2021-03-12 10:45:23 -0300

    reset styles

---
+++ src/styles/global.ts
@@ -0,0 +1,55 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1.6rem;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  p {
+    margin: 0px;
+  }
+
+  ul,
+  li {
+    list-style: none;
+    padding: 0px;
+    margin: 0px;
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+  }
+
+  button,
+  input {
+    display: block;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`
