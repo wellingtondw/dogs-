d3a0cb0f2f2f69bf52992b681abb33c6e780695a
wellington <wellingtonslpbi@hotmail.com>
2021-03-12 10:16:10 -0300

    add redux to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import {Provider } from 'react-redux'
+import store from './store';
+
 function App() {
   return (
+    <Provider store={store}>
     <h1>Hello World</h1>
+    </Provider>
   );
 }
 
